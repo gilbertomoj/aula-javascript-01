b86f521805a320efadd9bc6235f23222bd327ee4
gilbertomoj <gibamedeirosgc@gmail.com>
2021-09-17 23:15:14 -0300

    Teste

---
+++ index (1).js
@@ -0,0 +1,9 @@
+var nome = "GILBERTO"
+var turma = "FRONTEND 1"
+
+let qntAlunos = 32
+let turmas = ["FRONTEND1", "FRONTEND2","JAVA T1","JAVA T2"]
+
+const professora = "Clau"
+const idade = 20
+
---
+++ index (2).js
@@ -0,0 +1,7 @@
+var numb = 123
+var string = "Variável do tipo string"
+var isBoolean = true
+var array = ["Cavalo","Carro","Moto","Carteira"]
+var object = { a : "clau", b:"gabi"}
+var undefinedVar = undefined
+var nullvar = null
